2feeeed10cd47889f7829b4e6fec640bf08b4e3e
oskarskiold <93259511+oskarskiold@users.noreply.github.com>
2022-03-16 13:42:18 +0100

    Fixade massa små saker. Började på desktop-versionen av beondesidan

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
 const nav = document.getElementById('nav-links')
 const burger = document.getElementById('burger')
+const linje = document.getElementById('linje')
+
 
 function toggleMenu() {
     nav.classList.toggle('nav-active')
     burger.classList.toggle('crossed_line')
+    linje.classList.toggle('gone') 
 }
 
 burger.addEventListener('click', toggleMenu)
