c9c859b7c99b9f3a0a742439333b5ac2043f04ea
0skarskiold <oskarskioldlundquist@gmail.com>
2022-03-26 19:00:27 +0100

    Att-göra sidan
    Lade till och fixade alla bilder. Lade också till bakgrund.

--- js/script.js
+++ js/script.js
@@ -1,10 +1,16 @@
 const nav = document.getElementById('nav-links')
 const burger = document.getElementById('burger')
+const gullh = document.getElementById('besok_gullh')
+const vandra = document.getElementById('vandra_pa_harm')
+const upptack = document.getElementById('upptack')
 
 
 function toggleMenu() {
     nav.classList.toggle('nav-active')
     burger.classList.toggle('crossed_line')
+    gullh.classList.toggle('gone')
+    vandra.classList.toggle('gone')
+    upptack.classList.toggle('gone')
 }
 
 burger.addEventListener('click', toggleMenu)
