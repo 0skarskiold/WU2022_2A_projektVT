54db3d37a661fe7dc33ca23bd3d63a9e6c96f77b
0skarskiold <oskarskioldlundquist@gmail.com>
2022-03-30 20:31:19 +0200

    Gjorde mobilversionen för galleri-sidan

--- js/script.js
+++ js/script.js
@@ -14,3 +14,20 @@ function toggleMenu() {
 }
 
 burger.addEventListener('click', toggleMenu)
+
+
+// Denna kod är hämtad från internet. Den gör så att bildspelet fungerar. 
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("slideshow");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > x.length) {slideIndex = 1}
+  x[slideIndex-1].style.display = "block";
+  setTimeout(carousel, 5000); // Change image every 2 seconds
+}
