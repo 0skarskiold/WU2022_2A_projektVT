855bee4c2855b1a2a8f717bcf4603754b72796c0
0skarskiold <oskarskioldlundquist@gmail.com>
2022-05-27 14:38:06 +0200

    fixade

--- js/script_att_gora.js
+++ js/script_att_gora.js
@@ -4,7 +4,7 @@ const gullh = document.getElementById('besok_gullh')
 const vandra = document.getElementById('vandra_pa_harm')
 const upptack = document.getElementById('upptack')
 const wrapper = document.getElementsByTagName('body')[0];
-
+//Blev konstigt när de andra sidorna försökte hitta bilder som inte fanns, därför blev det ett eget js-dokument.  
 
 function toggleMenu() {
     nav.classList.toggle('nav-active')
